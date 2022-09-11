a4f3bfeff3df22793cd96bce9d4f8536b4fe8469
larry <larry.moyo9@gmail.com>
2022-09-11 11:43:44 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example/TA-Sol2
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.8.1
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "github.com/gin-gonic/gin"
+    "net/http"
+    "os"
+)
+
+func getDirList(context *gin.Context) {
+
+	files, err := ioutil.ReadDir("/home/larry/SoftwareEngineering/TA-Sol2")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+
+        type fullDirList struct {
+            Name        string `json:"name`
+            Size        int64 `json:"size"`
+            Attributes  os.FileMode  `json:"attributes"`
+        }
+    
+        var dirList = []fullDirList{
+            {Name: file.Name(), Size: file.Size(), Attributes: file.Mode()},
+        }
+
+        fmt.Println(file.Name(), file.Size() / 1000, file.Mode())
+        context.IndentedJSON(http.StatusOK, dirList)
+    }
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/", getDirList)
+    router.Run("localhost:8080")
+}
+
